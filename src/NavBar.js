f0cf66e16a522076264c9000f688c620c3a7f178
Michael <michael.daniels1013@gmail.com>
2022-07-20 11:26:00 -0500

    Changed NavBar links to white style

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,20 +2,26 @@ import './App.css';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
+
+    const NavStyle = {
+        color: 'white'
+    };
+
+
   return (
     <nav>
       <h3>LOGO</h3>
         <ul className='nav-links'>
-            <Link to='/about'>
+            <Link style={NavStyle} to='/about'>
                 <li>About</li>
             </Link>
-            <Link to='/housewives'>
+            <Link style={NavStyle} to='/housewives'>
                 <li>Housewives</li>
             </Link>
-            <Link to='/mycast'>
+            <Link style={NavStyle} to='/mycast'>
             <   li>MyCast</li>
             </Link>
-            <Link to='/form'>
+            <Link style={NavStyle} to='/form'>
                 <li>Form</li>
             </Link>
         </ul>
