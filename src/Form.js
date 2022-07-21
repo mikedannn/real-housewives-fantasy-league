974c236a93517b36c3a38e17ad363dc8edcf3142
Michael <michael.daniels1013@gmail.com>
2022-07-21 13:47:11 -0500

    Created Form with appropriate input boxes

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React from 'react';
 
 function Form() {
   return (
-    <div className="App">
-      <h1>Form Page</h1>
+    <div className="form">
+      <h2>Create New Housewife Card</h2>
+        <form>
+            <input type='text' name='name' placeholder='Housewife Name' />
+            <input type='text' name='image' placeholder='Add Image URL' />
+            <input type='text' name='city' placeholder='City of Origin' />
+            <input type='number' name='age' placeholder='Age' />
+            <button className='formButton' type='submit'>Create Card</button>
+        </form>
     </div>
   );
 }
