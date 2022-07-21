092710b6bf47c116d92fe65edbf59193d79d043b
Michael <michael.daniels1013@gmail.com>
2022-07-21 12:15:23 -0500

    Passed down keys for each housewife to individual housewife cards

--- src/HousewivesList.js
+++ src/HousewivesList.js
@@ -1,11 +1,16 @@
+import React from 'react';
+import HousewifeCard from './HousewifeCard';
 import './App.css';
 
-function HousewivesList() {
-  return (
-    <div className="App">
-      <h1>LIST OF cards</h1>
-    </div>
-  );
+function HousewivesList({ housewives }) {
+
+    const returnHousewives = housewives.map((housewife) => <HousewifeCard key={housewife.id} age={housewife.age} city={housewife.city} image={housewife.image} name={housewife.name}/>)
+    
+    return (
+        <ul className='cards'>
+            {returnHousewives}
+         </ul>
+    );
 }
 
 export default HousewivesList;
--- src/HousewivesPage.js
+++ src/HousewivesPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MyCast from './MyCast';
 import Form from './Form';
 import HousewivesList from './HousewivesList';
@@ -7,16 +7,22 @@ const BASE_URL = "http://localhost:3002/housewivesData"
 
 function HousewivesPage() {
 
+  const [housewives, setHousewives] = useState([]);
+
   useEffect(() => {
     fetch(BASE_URL)
     .then((response) => response.json())
-    .then((housewivesData) => console.log(housewivesData))
-  }, []) 
+    .then(setHousewives)
+  }, []); 
+
+  console.log(housewives)
 
   return (
-    <div>
-      <h1>HOUSEWIVES PAGE</h1>
-    </div>
+    <main>
+      <Form />
+      <MyCast />
+      <HousewivesList housewives={housewives} />
+    </main>
   );
 }
 
