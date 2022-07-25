2e562852bffec2dca7b892cabbb84ca21752ea7f
Michael <michael.daniels1013@gmail.com>
2022-07-25 13:36:56 -0500

    Adjusted image sizes on cards to be uniform size

--- src/Form.js
+++ src/Form.js
@@ -4,12 +4,12 @@ import React from 'react';
 function Form() {
   return (
     <div className="form">
-      <h2>Create New Housewife Card</h2>
+      <h3>Create New Housewife Card</h3>
         <form>
             <input type='text' name='name' placeholder='Housewife Name' />
             <input type='text' name='image' placeholder='Add Image URL' />
             <input type='text' name='city' placeholder='City of Origin' />
-            <input type='number' name='age' placeholder='Age' />
+            <input type='text' name='age' placeholder='Age' />
             <button className='formButton' type='submit'>Create Card</button>
         </form>
     </div>
