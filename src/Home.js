e18fb23236d76515e12cbf6a2020cac391c30680
Michael <michael.daniels1013@gmail.com>
2022-07-27 13:36:38 -0500

    Card buttons toggle between Add to My Cast and In My Cast when clicked

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import './App.css';
 function Home() {
     return (
       <div className="App">
-        <h1>WELCOME TO THE HOUSEWIVES APP</h1>
+        <h1 className='header'>Welcome to The Real Housewives Fantasy League!</h1>
+        <img className='logo' src='https://www.tvguide.com/a/img/catalog/provider/1/1/1-172545467.jpg'/>
       </div>
     );
 }
--- src/HousewifeCard.js
+++ src/HousewifeCard.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function HousewifeCard({ age, city, image, name }) {
+  const [addToCast, setAddToCast] = useState(true);
+
+  function handleClick(newClick) {
+    console.log('clicked');
+    setAddToCast(newClick);
+  }
+
   return (
 
     <li className="card">
@@ -9,7 +16,11 @@ function HousewifeCard({ age, city, image, name }) {
         <h3>{name}</h3>
         <p>City: {city} </p>
         <p>Age: {age} </p>
-        <button>Add to My Cast!</button>
+        {addToCast ? (
+          <button className='addButton' onClick={() => handleClick(false)}>Add to My Cast!</button>
+        ) : (
+          <button className='inCastButton' onClick={() => handleClick(true)}>In My Cast!</button>
+        )}
     </li>
   );
 }
