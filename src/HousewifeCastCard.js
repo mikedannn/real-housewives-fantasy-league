845b65e28a62ab9bd8ea2c0fcf86567268962945
Michael <michael.daniels1013@gmail.com>
2022-08-08 20:58:05 -0500

    In cast div working

--- src/HousewifeCastCard.js
+++ src/HousewifeCastCard.js
@@ -1,29 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
 
-// function HousewifeCard({ id, age, city, image, name, likes, casted, updateHousewife, addMyHousewife }) {
 function HousewifeCastCard({ key, housewife }) {
 
   const BASE_URL = "http://localhost:3002/housewivesData"
-
-//   function handleClick(newClick) {
-//     setAddToCast(newClick);
-//   }
-
-//   function addLike(e) {
-
-//     const housewifeLikes = {
-//       method: "PATCH", 
-//       headers: {
-//         "Content-type": "application/json"
-//       },
-//       body: JSON.stringify({"likes": housewife.likes + 1})
-//     }
-//     // fetch(`${BASE_URL}/${id}`, housewifeLikes)
-//     fetch(`${BASE_URL}/${housewife.id}`, housewifeLikes)
-//     .then((response) => response.json())
-//     .then((data) => updateHousewife(data))
-//   }
 
   return (
 
@@ -32,8 +12,7 @@ function HousewifeCastCard({ key, housewife }) {
         <h2>{housewife.name}</h2>
         <p>City: {housewife.city} </p>
         <p>Age: {housewife.age}</p>
-        <button className='inCastButton' onClick={console.log('clicked')}>In My Cast!</button>
-        
+        <div className='inCastDiv'>In My Cast!</div>
     </li>
   );
 }
